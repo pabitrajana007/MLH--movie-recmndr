5916a2886c426bd430ea032430c5af6013cac517
pabitrajana007 <pabitranewjana@gmail.com>
2024-08-10 15:22:09 +0530

    port changes

--- backend/main.py
+++ backend/main.py
@@ -76,4 +76,5 @@ if __name__ == "__main__":
     similarity = cosine_similarity(feature_vectors)
     
     # Run Flask app
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
